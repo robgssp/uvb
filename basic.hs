1e5e36f0d6f2ad985921d144d2c4f042774b2052
Robert Glossop <robgssp@gmail.com>
2014-08-28 14:39:38 -0400

    updates

--- basic.hs
+++ basic.hs
@@ -9,9 +9,17 @@ import GHC.IO.Handle
 main = do
   sh <- connectTo "jake.csh.rit.edu" $ PortNumber 8080
   B.hPutStr sh "POST /register/robgssp HTTP/1.1\n\n"
+  forM_ ([0..98]::[Int]) $ \_ -> forkIO attack
+  attack1 sh
+
+attack = (connectTo "jake.csh.rit.edu" $ PortNumber 8080) >>= attack1
+
+attack1 sh = do
   forkIO $ do
     let sz = 1024*1024
     buf <- mallocBytes sz
-    forever $ do hGetBuf sh buf sz >> return () 
-  forever $ B.hPutStr sh "POST /robgssp HTTP/1.1\n\n"
-  
+    forever $ hGetBuf sh buf sz >> return () 
+  forever $ B.hPutStr sh prepmsg
+
+prepmsg :: B.ByteString
+prepmsg = B.concat $ take 64 $ repeat "POST /robgssp HTTP/1.1\n\n"
